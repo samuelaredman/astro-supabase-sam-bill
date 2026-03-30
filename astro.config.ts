f1f7e4073f98c90bb17d2f2c3b4f4bbb113bac72
samuelaredman <samuelaredman@gmail.com>
2026-03-30 11:25:13 -0500

    Update astro.config.ts
    added absolute alias supabase import

--- astro.config.ts
+++ astro.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 import netlify from '@astrojs/netlify';
 import tailwindcss from "@tailwindcss/vite";
+import { supabase } from "@lib/supabase";
 
 export default defineConfig({
   markdown: {
