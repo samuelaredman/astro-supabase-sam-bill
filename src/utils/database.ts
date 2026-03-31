f52b0b4f660bc4574491a4c734eca66eddefcf8a
samuelaredman <samuelaredman@gmail.com>
2026-03-31 13:11:01 -0500

    Update database.ts
    added createServerClient for authentication purposes

--- src/utils/database.ts
+++ src/utils/database.ts
@@ -1,7 +1,32 @@
 import { createClient } from '@supabase/supabase-js'
+import { createServerClient, parseCookieHeader, serializeCookieHeader } from '@supabase/ssr';
 import type { Database } from '../../supabase/types'
 
 const supabaseUrl = import.meta.env.SUPABASE_DATABASE_URL
 const supabaseKey = import.meta.env.SUPABASE_ANON_KEY
 
-export const supabase = supabaseUrl && supabaseKey ? createClient<Database>(supabaseUrl, supabaseKey) : null;
+export const supabase = supabaseUrl && supabaseKey
+  ? createClient<Database>(supabaseUrl, supabaseKey)
+  : null;
+
+export function createSupabaseServerClient(request: Request, response: Response) {
+  return createServerClient(
+    import.meta.env.SUPABASE_DATABASE_URL,
+    import.meta.env.SUPABASE_ANON_KEY,
+    {
+      cookies: {
+        getAll() {
+          return parseCookieHeader(request.headers.get('Cookie') ?? '');
+        },
+        setAll(cookiesToSet) {
+          cookiesToSet.forEach(({ name, value, options }) => {
+            response.headers.append(
+              'Set-Cookie',
+              serializeCookieHeader(name, value, options)
+            );
+          });
+        },
+      },
+    }
+  );
+}
