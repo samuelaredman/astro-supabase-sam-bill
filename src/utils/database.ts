e17aceaf171c4390ed8620798763e60ba8d8fc53
samuelaredman <samuelaredman@gmail.com>
2026-03-31 13:27:00 -0500

    Update database.ts
    potentially fixed imports and setAll(cookiesToSet)

--- src/utils/database.ts
+++ src/utils/database.ts
@@ -1,6 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
-import { createServerClient, parseCookieHeader, serializeCookieHeader } from '@supabase/supabase-js';
+import { createServerClient, parseCookieHeader, serializeCookieHeader } from '@supabase/ssr';
 import type { Database } from '../../supabase/types'
+import type { CookieOptions } from '@supabase/ssr';
+
 
 const supabaseUrl = import.meta.env.SUPABASE_DATABASE_URL
 const supabaseKey = import.meta.env.SUPABASE_ANON_KEY
@@ -18,7 +20,7 @@ export function createSupabaseServerClient(request: Request, response: Response)
         getAll() {
           return parseCookieHeader(request.headers.get('Cookie') ?? '');
         },
-        setAll(cookiesToSet) {
+        setAll(cookiesToSet: { name: string; value: string; options: CookieOptions }[]) {
           cookiesToSet.forEach(({ name, value, options }) => {
             response.headers.append(
               'Set-Cookie',
