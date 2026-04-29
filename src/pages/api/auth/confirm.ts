cd6e59be524a844adc3c13d068886fda2edc1a93
Samuel Redman <samuelaredman@gmail.com>
2026-04-29 14:10:02 -0700

    Update confirm.ts

--- src/pages/api/auth/confirm.ts
+++ src/pages/api/auth/confirm.ts
@@ -1,23 +1,24 @@
 export const prerender = false;
-
-import { createSupabaseServerClient } from "../../../utils/database";
+import { createSupabaseServerClientFromContext } from "../../../utils/database";
 import type { APIRoute } from "astro";
 import type { EmailOtpType } from "@supabase/supabase-js";
 
-export const GET: APIRoute = async ({ request, redirect }) => {
-  const response = new Response();
-  const supabase = createSupabaseServerClient(request, response);
-
-  const url = new URL(request.url);
+export const GET: APIRoute = async (context) => {
+  const supabase = createSupabaseServerClientFromContext(context);
+  const url = new URL(context.request.url);
   const token_hash = url.searchParams.get("token_hash");
   const type = url.searchParams.get("type") as EmailOtpType | null;
+  const code = url.searchParams.get("code");
+
+  if (code) {
+    const { error } = await supabase.auth.exchangeCodeForSession(code);
+    if (!error) return context.redirect("/");
+  }
 
   if (token_hash && type) {
     const { error } = await supabase.auth.verifyOtp({ type, token_hash });
-    if (!error) {
-      return redirect("/");
-    }
+    if (!error) return context.redirect("/");
   }
 
-  return redirect("/signin?error=confirmation_failed");
+  return context.redirect("/signin?error=confirmation_failed");
 };
